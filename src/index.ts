e1c54f0c8781a74d0b15a8560751dda737aacfe5
dhananjay maurya <dhanuverma9721@gmail.com>
2024-01-27 12:18:50 +0530

    init 3

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+// import { config } from "dotenv";
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+// import express from "express";
+
+config();
+// const express = require('express');
+// const cors = require('cors');
 
 const app = express();
 app.use(cors());
+// app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
@@ -16,14 +23,14 @@ function corsMiddleware(req, res, next) {
 
 app.use(corsMiddleware);
 
-app.get('/', (req, res) =>{
-    res.send({title:"Welcome"})
+app.get('/', (req, res) => {
+    res.send({ title: "Welcome" })
 })
-app.get('/data', (req, res) =>{
-    res.send({data:"dhananjay"})
+app.get('/data', (req, res) => {
+    res.send({ data: "dhananjay" })
 })
 
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
-} )
+})
