e02d749db70ce0f7a5f3ef81b2f0fd240bf2983e
dhananjay maurya <dhanuverma9721@gmail.com>
2024-01-27 11:18:03 +0530

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) =>{
+    res.send({title:"Welcome"})
+})
+
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+} )
